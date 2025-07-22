d26ef3ba2231954af5f742ce0912daf19c38b1e3
ItsSitanshu <sitanshu15shrestha@gmail.com>
2025-07-23 00:05:34 +0545

    Final

--- jhon-team/app/__init__.py
+++ jhon-team/app/__init__.py
@@ -7,17 +7,36 @@ from .config import Config
 db = SQLAlchemy()
 ma = Marshmallow()
 
+swagger_config = {
+    'title': 'Jhon Team API', 
+    'uiversion': 3,
+    'openapi': '3.0.2',
+    'specs': [
+        {
+            'endpoint': 'apispec_1',
+            'route': '/swagger.json',
+            'rule_filter': lambda rule: True,  
+            'model_filter': lambda tag: True,  
+        }
+    ],
+    'headers': [],
+    'static_url_path': '/flasgger_static',
+    'swagger_ui': True,
+    'specs_route': '/docs/'
+}
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
     ma.init_app(app)
-    
-    # Swagger(app)
+    Swagger(app, config=swagger_config)
 
     with app.app_context():
         from .v1 import api_v1_bp
+        
+        db.create_all()
         app.register_blueprint(api_v1_bp, url_prefix='/api/v1')
     
     return app
--- jhon-team/run.py
+++ jhon-team/run.py
@@ -1,8 +1,6 @@
 from app import create_app, db
-from flask_migrate import Migrate
-from app.models import User, QuestionPaper, Question
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001)
